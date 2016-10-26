10c075f64b00f1f6d24fbc73043c9bc1ab08952c
YihengYao <fn1117@qq.com>
2016-10-26 22:25:49 +1300

    Autolayout的练习

---
+++ 16_10_25_Autolayout的练习/16_10_25_Autolayout的练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  16_10_25_Autolayout的练习
+//
+//  Created by 姚懿恒 on 2016/10/25.
+//  Copyright © 2016年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 16_10_25_Autolayout的练习/16_10_25_Autolayout的练习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  16_10_25_Autolayout的练习
+//
+//  Created by 姚懿恒 on 2016/10/25.
+//  Copyright © 2016年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
