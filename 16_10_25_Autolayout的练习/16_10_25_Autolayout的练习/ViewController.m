0983c5df24d90f1f055686739bdc5845cebb81b7
YihengYao <fn1117@qq.com>
2016-11-01 17:37:16 +1300

    想要不使用前缀mas_
    如果想要不使用前缀mas_, 可在导入的框架的头文件之前设置两个宏

--- 16_10_25_Autolayout的练习/16_10_25_Autolayout的练习/ViewController.m
+++ 16_10_25_Autolayout的练习/16_10_25_Autolayout的练习/ViewController.m
@@ -7,6 +7,13 @@
 //
 
 #import "ViewController.h"
+
+//define this constant if you want to use Masonry without the 'mas_' prefix
+#define MAS_SHORTHAND
+
+//define this constant if you want to enable auto-boxing for default syntax
+#define MAS_SHORTHAND_GLOBALS
+
 #import "Masonry.h"
 
 @interface ViewController ()
@@ -67,6 +74,8 @@
 
     UIView *redView = [UIView new];
     redView.backgroundColor = [UIColor redColor];
+
+    redView.translatesAutoresizingMaskIntoConstraints = NO;
 
     [self.view addSubview:redView];
 
