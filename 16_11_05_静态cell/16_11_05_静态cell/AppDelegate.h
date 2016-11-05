6939ae2412290303678d2b7c9727434c02928199
YihengYao <fn1117@qq.com>
2016-11-05 18:59:27 +1300

    静态cell

---
+++ 16_11_05_静态cell/16_11_05_静态cell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  16_11_05_静态cell
+//
+//  Created by 姚懿恒 on 2016/11/5.
+//  Copyright © 2016年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 16_11_05_静态cell/16_11_05_静态cell/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  16_11_05_静态cell
+//
+//  Created by 姚懿恒 on 2016/11/5.
+//  Copyright © 2016年 姚懿恒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
