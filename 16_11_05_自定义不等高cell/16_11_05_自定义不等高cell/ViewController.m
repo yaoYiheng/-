54653e4958ab7615078a0858b25ab981c9463efc
YihengYao <fn1117@qq.com>
2016-11-08 11:35:16 +1300

    计算不等高cell
    需要在- (CGFloat)tableView:(UITableView *)tableView
    heightForRowAtIndexPath:(NSIndexPath *)indexPath 方法中计算每一行cell的高度.

--- 16_11_05_自定义不等高cell/16_11_05_自定义不等高cell/ViewController.m
+++ 16_11_05_自定义不等高cell/16_11_05_自定义不等高cell/ViewController.m
@@ -30,6 +30,7 @@ NSString *ID = @"weibo";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     // Do any additional setup after loading the view, typically from a nib.
     [self.tableView registerClass:[YHStatuesCell class] forCellReuseIdentifier:ID];
     NSLog(@"%@", _statusData);
@@ -49,6 +50,56 @@ NSString *ID = @"weibo";
     cell.statuses = self.statusData[indexPath.row];
 
     return cell;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    //声明高度初始化为0
+    CGFloat cellHeight = 0;
+
+    //拿到对应行的Status
+    YHStatus *statuses = self.statusData[indexPath.row];
+
+    /** 重新计算影响cell高度的子控件并返回新高度.*/
+    CGFloat space = 10;
+
+    /**头像图片 */
+    CGFloat iconX = space;
+    CGFloat iconY = space;
+    CGFloat iconWH = 30;
+    CGRect iconImageViewFrame = CGRectMake(iconX, iconY, iconWH, iconWH);
+
+
+
+    /**微博内容 */
+    CGFloat contentX = iconX;
+    CGFloat contentY = CGRectGetMaxY(iconImageViewFrame) + space;
+    CGFloat contentW = self.view.frame.size.width - 2 *space;
+
+    NSDictionary *contentAttribute = @{NSFontAttributeName: [UIFont systemFontOfSize:14]};
+    CGSize contentSize = CGSizeMake(contentW, MAXFLOAT);
+
+
+    CGFloat contentH = [statuses.text boundingRectWithSize:contentSize options:NSStringDrawingUsesLineFragmentOrigin attributes:contentAttribute context:nil].size.height;
+
+    CGRect contentLabelFrame = CGRectMake(contentX, contentY, contentW, contentH);
+
+
+    /**微博图片 */
+    if (statuses.picture) {
+        CGFloat pictureX = space;
+        CGFloat pictureY = CGRectGetMaxY(contentLabelFrame) + space;
+        CGFloat pictureWH = 150;
+        CGRect pictureImageViewFrame = CGRectMake(pictureX, pictureY, pictureWH, pictureWH);
+
+        //如果有图片, cell的高度为图片最大Y值+间距
+        cellHeight = CGRectGetMaxY(pictureImageViewFrame) + space;
+    }else
+    {   //没有图片, 正文内容加上间距
+        cellHeight = CGRectGetMaxY(contentLabelFrame) + space;
+    }
+
+
+    return cellHeight;
 }
 
 @end
--- 16_11_05_自定义不等高cell/16_11_05_自定义不等高cell/YHStatuesCell.m
+++ 16_11_05_自定义不等高cell/16_11_05_自定义不等高cell/YHStatuesCell.m
@@ -17,7 +17,7 @@
 /** 内容*/
 @property (nonatomic, weak) UILabel *contentLabel;
 /** 图片*/
-@property (nonatomic, weak) UIImageView *picutreImageView;
+@property (nonatomic, weak) UIImageView *pictureImageView;
 /** Vip*/
 @property (nonatomic, weak) UIImageView *vipImageView;
 
@@ -41,23 +41,27 @@
         /**图片 */
         UIImageView *picutreImageView = [[UIImageView alloc] init];
         [self.contentView addSubview:picutreImageView];
-        self.picutreImageView = picutreImageView;
+        self.pictureImageView = picutreImageView;
 
         /**VIP */
         UIImageView *vipImageView = [[UIImageView alloc] init];
         vipImageView.image = [UIImage imageNamed:@"vip"];
+        //设置图片居中显示
+        vipImageView.contentMode = UIViewContentModeCenter;
         [self.contentView addSubview:vipImageView];
         self.vipImageView = vipImageView;
 
         /**昵称 */
         UILabel *nameLabel = [[UILabel alloc] init];
-        //
-        nameLabel.font = [UIFont systemFontOfSize:17];
+        //设置显示的字体大小
+        nameLabel.font = [UIFont systemFontOfSize:14];
         [self.contentView addSubview:nameLabel];
         self.nameLabel = nameLabel;
 
         /**微博内容 */
         UILabel *contentLabel = [[UILabel alloc] init];
+        contentLabel.numberOfLines = 0;
+        contentLabel.font = [UIFont systemFontOfSize:14];
         [self.contentView addSubview:contentLabel];
         self.contentLabel = contentLabel;
     }
@@ -88,12 +92,35 @@
     self.nameLabel.frame = CGRectMake(nameX, nameY, nameW, nameH);
 
     /**微博内容 */
+    CGFloat contentX = iconX;
+    CGFloat contentY = CGRectGetMaxY(self.iconImageView.frame) + space;
+    CGFloat contentW = self.contentView.frame.size.width - 2 *space;
+
+    NSDictionary *contentAttribute = @{NSFontAttributeName: [UIFont systemFontOfSize:14]};
+    CGSize contentSize = CGSizeMake(contentW, MAXFLOAT);
+
+
+    CGFloat contentH = [self.statuses.text boundingRectWithSize:contentSize options:NSStringDrawingUsesLineFragmentOrigin attributes:contentAttribute context:nil].size.height;
+
+    self.contentLabel.frame = CGRectMake(contentX, contentY, contentW, contentH);
 
     /**VIP */
-    CGFloat vipX = CGRectGetMaxX(self.nameLabel.frame) + space;
-    CGFloat vipY = 
+    if (self.statuses.vip) {
+        CGFloat vipX = CGRectGetMaxX(self.nameLabel.frame) + space;
+        CGFloat vipY = nameY;
+        CGFloat vipW = 14;
+        CGFloat vipH = nameH;
+        self.vipImageView.frame = CGRectMake(vipX, vipY, vipW, vipH);
+    }
+
 
     /**微博图片 */
+    if (self.pictureImageView) {
+        CGFloat pictureX = space;
+        CGFloat pictureY = CGRectGetMaxY(self.contentLabel.frame) + space;
+        CGFloat pictureWH = 150;
+        self.pictureImageView.frame = CGRectMake(pictureX, pictureY, pictureWH, pictureWH);
+    }
 
 
 }
@@ -112,6 +139,13 @@
     }else{
         self.vipImageView.hidden = YES;
         self.nameLabel.textColor = [UIColor blackColor];
+    }
+    if (statuses.picture) {
+        self.pictureImageView.hidden = NO;
+        self.pictureImageView.image = [UIImage imageNamed:statuses.picture];
+    }
+    else{
+        self.pictureImageView.hidden = YES;
     }
 
 }
