6b39af1fc02a3d5d67ef5278b7f93d474c62f4de
YihengYao <fn1117@qq.com>
2016-11-11 23:03:53 +1300

    实现批量选择与删除功能

--- 16_11_09_UITableView刷新数据/16_11_09_UITableView刷新数据/ViewController.m
+++ 16_11_09_UITableView刷新数据/16_11_09_UITableView刷新数据/ViewController.m
@@ -15,6 +15,7 @@
 
 /** 酒模型数组*/
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *deleteButton;
 @property (nonatomic, strong) NSMutableArray *wineArray;
 @end
 
@@ -30,11 +31,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    //调用该属性允许在进入编辑模式时选择多行
+    self.tableView.allowsMultipleSelectionDuringEditing = YES;
 
-
-//    [self.tableView registerClass:[YHWineCell class] forCellReuseIdentifier:ID];
-
+    self.deleteButton.enabled = NO;
 }
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return self.wineArray.count;
@@ -47,24 +47,50 @@
     cell.wine = self.wineArray[indexPath.row];
     return cell;;
 }
-- (IBAction)addOrRefreshOrDelete:(UIBarButtonItem *)sender {
-    NSLog(@"%@", sender);
+- (IBAction)delete:(UIBarButtonItem *)sender
+{
+    //创建一个数组用来保存选中的元素
+    NSMutableArray *selectedItems = [NSMutableArray array];
+
+    //变量所选中行的 indexPath, 并通过indexPath拿到所对应的模型, 并添加到数组中.
+    for (NSIndexPath *indextPath in self.tableView.indexPathsForSelectedRows) {
+
+        [selectedItems addObject:self.wineArray[indextPath.row]];
+
+    }
+    //调用 removeObjectsInArray:方法 从数组中删除 保存元素的数组
+    [self.wineArray removeObjectsInArray:selectedItems];
+
+    //调用 deleteRowsAtIndexPaths:方法 从tableView中删除对应的cell(完成刷新)
+    [self.tableView deleteRowsAtIndexPaths:self.tableView.indexPathsForSelectedRows withRowAnimation:UITableViewRowAnimationAutomatic];
+
+
 }
 
-#pragma mark 调用该代理方法, 就能实现向左滑动出现删除按钮功能
--(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
-    //通过修改模型数据达到修改界面的目的. 删除所选择的那一行.
-    [self.wineArray removeObjectAtIndex:indexPath.row];
+- (IBAction)multiDelete:(UIBarButtonItem *)sender {
 
-    /**在数组中删除模型后, 需对tableView进行刷新, 
-     这里使用deleteRowsAtIndexPaths:进行局部刷新, 并实现动画.
-     */
-    [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    //设置进入编辑模式 是否进入编辑模式取决于tableView是否处于编辑模式.
+    [self.tableView setEditing:!self.tableView.isEditing animated:YES];
+
+    //根据是否进入编辑模式判断按钮是否可用
+    self.deleteButton.enabled =self.tableView.editing;
+
 }
-#pragma mark 调用该方法, 可更改按钮显示的文字.
-- (nullable NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return @"删除";
-}
+
+//#pragma mark 调用该代理方法, 就能实现向左滑动出现删除按钮功能
+//-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+//    //通过修改模型数据达到修改界面的目的. 删除所选择的那一行.
+//    [self.wineArray removeObjectAtIndex:indexPath.row];
+//
+//    /**在数组中删除模型后, 需对tableView进行刷新, 
+//     这里使用deleteRowsAtIndexPaths:进行局部刷新, 并实现动画.
+//     */
+//    [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+//}
+//#pragma mark 调用该方法, 可更改按钮显示的文字.
+//- (nullable NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    return @"删除";
+//}
 #pragma mark 调用该方法, 可为向左滑动增加新按钮与想要实现的功能
 - (NSArray<UITableViewRowAction *> *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(nonnull NSIndexPath *)indexPath{
 
