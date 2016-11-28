c444fee22117da80a5286af9f981f8aa77df06ec
YihengYao <fn1117@qq.com>
2016-11-29 06:59:05 +1300

    控制器之间的跳转

---
+++ 16_11_28_导航控制器的基本使用及跳转/YHViewControllerTwo.h
@@ -0,0 +1,13 @@
+//
+//  YHViewControllerTwo.h
+//  16_11_28_导航控制器的基本使用及跳转
+//
+//  Created by 姚懿恒 on 2016/11/29.
+//  Copyright © 2016年 姚懿恒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface YHViewControllerTwo : ViewController
+
+@end
--- 16_11_28_导航控制器的基本使用及跳转/YHViewControllerTwo.m
+++ 16_11_28_导航控制器的基本使用及跳转/YHViewControllerTwo.m
@@ -1,19 +1,19 @@
 //
-//  YHViewControllerOne.m
+//  YHViewControllerTwo.m
 //  16_11_28_导航控制器的基本使用及跳转
 //
-//  Created by 姚懿恒 on 2016/11/28.
+//  Created by 姚懿恒 on 2016/11/29.
 //  Copyright © 2016年 姚懿恒. All rights reserved.
 //
 
-#import "YHViewControllerOne.h"
 #import "YHViewControllerTwo.h"
-
-@interface YHViewControllerOne ()
+#import "YHRootViewController.h"
+#import "YHViewControllerOne.h"
+@interface YHViewControllerTwo ()
 
 @end
 
-@implementation YHViewControllerOne
+@implementation YHViewControllerTwo
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,9 +24,19 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)jumpToNext {
-    YHViewControllerTwo *twoVc = [[YHViewControllerTwo alloc] init];
-    [self.navigationController pushViewController:twoVc animated:YES];
+
+/**
+ 通过该方法跳转到跟控制器.
+ */
+- (IBAction)jumpToRoot {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+- (IBAction)jumpToSecond {
+    //通过该方法可跳转到指定的控制器, 但必须是导航控制器的自控制.
+    [self.navigationController popToViewController:self.navigationController.childViewControllers[1] animated:YES];
+
+    //通过该方法, 跳转到上一个控制器.
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 /*
