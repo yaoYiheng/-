871dce2ecdee4960509d6be7290103b2c5b85c40
YihengYao <fn1117@qq.com>
2016-12-18 10:44:20 +1300

    事件传递的全过程
    application接收到事件后一层层的向下传递
    application->UIWindow->控制器VIew->View的子控件, 如果没有控件响应该事件,
    则将该事件传递给该控件的父控件,(会在传递前判断当前view是不是控制器View, 如果是, 控制器就是上一个响应者,
    如果不是,其父控件就是上一个响应者), 如果事件再次传到application, 则没有控件相应该事件,
    application会将该事件抛弃.

---
+++ 16_12_15_寻找最适合的view/16_12_15_寻找最适合的view/redView.h
@@ -0,0 +1,13 @@
+//
+//  redView.h
+//  16_12_15_寻找最适合的view
+//
+//  Created by 姚懿恒 on 2016/12/18.
+//  Copyright © 2016年 姚懿恒. All rights reserved.
+//
+
+#import "basicView.h"
+
+@interface redView : UIView
+
+@end
--- 16_12_15_寻找最适合的view/16_12_15_寻找最适合的view/redView.m
+++ 16_12_15_寻找最适合的view/16_12_15_寻找最适合的view/redView.m
@@ -1,14 +1,14 @@
 //
-//  blueView.m
+//  redView.m
 //  16_12_15_寻找最适合的view
 //
-//  Created by 姚懿恒 on 2016/12/15.
+//  Created by 姚懿恒 on 2016/12/18.
 //  Copyright © 2016年 姚懿恒. All rights reserved.
 //
 
-#import "blueView.h"
+#import "redView.h"
 
-@implementation blueView
+@implementation redView
 
 /*
 // Only override drawRect: if you perform custom drawing.
@@ -17,8 +17,5 @@
     // Drawing code
 }
 */
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    NSLog(@"%s", __func__);
-}
 
 @end
