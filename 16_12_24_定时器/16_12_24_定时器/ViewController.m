4b4ad4c5232e92ca231607d94052d56b8a28ba7c
YihengYao <fn1117@qq.com>
2016-12-25 23:31:18 +1300

    16_12_25_设置图片的裁剪
    总结:刚出现一个小bug, 在裁剪完之后周围出现了黑色, 仔细检查后发现是
    UIGraphicsBeginImageContextWithOptions(originalImage.size, NO, 0.0);
    中第二个参数设置为yes, 为透明, 所以呈现出黑色- -.

--- 16_12_24_定时器/16_12_24_定时器/ViewController.m
+++ 16_12_24_定时器/16_12_24_定时器/ViewController.m
@@ -18,26 +18,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //0. 加载图片
-    UIImage *originalImage = [UIImage imageNamed:@"Image009"];
+    UIImage *originalImage = [UIImage imageNamed:@"Gin_1.jpeg"];
     //1. 获取位图上下文
-    UIGraphicsBeginImageContextWithOptions(originalImage.size, YES, 0.0);
+    //上下文的大小和原始图片保持一样,以免图片被拉伸缩放.
+    //opaque:不透明度,当为YES为不透明, 为NO的时候透明,
+    UIGraphicsBeginImageContextWithOptions(originalImage.size, NO, 0.0);
+
+    /**
+     实现裁剪效果, 需要先设置裁剪区域, 在进行图片以及水印的绘制, 才能实现裁剪的效果.
+     */
+    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, originalImage.size.width, originalImage.size.height)];
+    //添加路径为裁剪区域.
+    [path addClip];
+
     //2. 将图片绘制到上下文中
     [originalImage drawAtPoint:CGPointZero];
 
     //3. 设置想要显示的文字, 以及设置文字的相关属性, 通过字典的形式.
-    NSString *logo = @"By Yiheng";
-    NSMutableDictionary *logoAttribute = [NSMutableDictionary dictionary];
-    logoAttribute[NSFontAttributeName] = [UIFont systemFontOfSize:50];
-    logoAttribute[NSForegroundColorAttributeName] = [UIColor orangeColor];
-    //4. 将文字绘制到上下文中
-    [logo drawAtPoint:CGPointZero withAttributes:logoAttribute];
+//    NSString *logo = @"By Yiheng";
+//    NSMutableDictionary *logoAttribute = [NSMutableDictionary dictionary];
+//    logoAttribute[NSFontAttributeName] = [UIFont systemFontOfSize:50];
+//    logoAttribute[NSForegroundColorAttributeName] = [UIColor orangeColor];
+//    //4. 将文字绘制到上下文中
+//    [logo drawAtPoint:CGPointMake(300, 700) withAttributes:logoAttribute];
+
 
     //5. 调用UIGraphicsGetImageFromCurrentImageContext()返回一张已经生成图片与水印的新图
     UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-    self.imageView.image = newImage;
 
     //6. 由于是手动获取位图上下文, 需要在结束后关闭图形上下文
     UIGraphicsEndImageContext();
+    
+    self.imageView.image = newImage;
+
+
 }
 
 
