96d51ae7f387eec2924cdcee4258214ec1285c4a
YihengYao <fn1117@qq.com>
2016-12-30 23:05:23 +1300

    16_12_30_连线以及显示弹窗
    注意点1. 在- (void)touchesMoved:(NSSet<UITouch *> *)touches
    withEvent:(UIEvent *)event 在拖动的过程中就需要重绘, 而不是在判断添加的时候才进行重绘,
    并且将当前手指的点赋值给成员变量. 在- (void)drawRect:(CGRect)rect方法中, 在连接了所以按钮后,
    在添加一根线到手指当前位置.
    
    注意点2. 由于UIAlertView在IOS8之后废弃, 所以采用UIAlertController, 但是这个只能在控制器对象中使用,
    想要在UIView对象中使用, 需要先拿到当前控制器对象, 在stackoverflow中找到解决方法
    - (UIViewController *)currentTopViewController {
        UIViewController *topVC = [[[[UIApplication sharedApplication]
    delegate] window] rootViewController];
        while (topVC.presentedViewController) {
            topVC = topVC.presentedViewController;
        }
        return topVC;
    }

--- 16_12_29_手势解锁/16_12_29_手势解锁/UnlockView.m
+++ 16_12_29_手势解锁/16_12_29_手势解锁/UnlockView.m
@@ -8,7 +8,23 @@
 
 #import "UnlockView.h"
 
+
+@interface UnlockView ()
+/** 被选中的按钮*/
+@property (nonatomic, strong) NSMutableArray *selectedButton;
+/** 用来记录当前手指的点*/
+@property (nonatomic, assign) CGPoint currentPoint;
+@end
+
 @implementation UnlockView
+
+- (NSMutableArray *)selectedButton{
+    if (_selectedButton == nil) {
+        _selectedButton = [NSMutableArray array];
+    }
+    return _selectedButton;
+}
+
 #pragma mark 从xib加载初始化
 - (void)awakeFromNib{
     [super awakeFromNib];
@@ -28,8 +44,16 @@
     for (int i = 0; i < 9; i++)
     {
         UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+
         [button setImage: [UIImage imageNamed:@"gesture_node_selected"] forState:UIControlStateSelected];
+        
         [button setImage:[UIImage imageNamed:@"gesture_node_normal"] forState:UIControlStateNormal];
+
+        //为了方便记录拖线的顺序, 在这里为每一个button绑定tag
+        button.tag = i;
+
+
+        //为了能是button的父控件接收点击事件, 能够去到touch相关的方法, 需要将按钮的userInteractionEnabled设置为NO.
         button.userInteractionEnabled = NO;
         [self addSubview:button];
 
@@ -80,24 +104,101 @@
 
     CGPoint currentPoint = [self getCurrentPoint:touches];
 
+
+
     //如果手指点在按钮上就将按钮设置为选中状态
     UIButton *button = [self buttonContainsPoint:currentPoint];
-    if (button) {
+    if (button && button.selected == NO) {
         button.selected = YES;
+        [self.selectedButton addObject:button];
     }
 
 }
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     CGPoint currentPoint = [self getCurrentPoint:touches];
 
+
     //如果手指点在按钮上就将按钮设置为选中状态
     UIButton *button = [self buttonContainsPoint:currentPoint];
-    if (button) {
+    if (button && button.selected == NO) {
         button.selected = YES;
+        [self.selectedButton addObject:button];
+
     }
 
+    //只要来带该方法就进行重绘.
+    [self setNeedsDisplay];
+    //获取当前手指所在点,
+    self.currentPoint = currentPoint;
 }
+#pragma mark 手指离开屏幕来带该方法
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSMutableString *selectedString = [NSMutableString string];
+    //所有选中状态的按钮取消选中
+    for (UIButton *button in self.selectedButton) {
+        button.selected = NO;
+        [selectedString appendFormat:@"%ld", button.tag];
+    }
+    //移除所有选中的按钮
+    [self.selectedButton removeAllObjects];
+    //重新绘制
+    [self setNeedsDisplay];
+
+
+    //在UIView中使用 UIAlertController
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"手势顺序为" message:selectedString preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction *confirm = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:nil];
+
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+    [alertController addAction:confirm];
+    [alertController addAction:cancel];
+
+    //获取当前控制器.
+    UIViewController *currentViewController = [self currentTopViewController];
+
+    //显示alertController
+    [currentViewController presentViewController:alertController animated:YES completion:^{
+
+    }];
+
+
+}
+#pragma mark 在View中获取当前控制器对象
+- (UIViewController *)currentTopViewController {
+    UIViewController *topVC = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    while (topVC.presentedViewController) {
+        topVC = topVC.presentedViewController;
+    }
+    return topVC;
+}
+
+- (void)drawRect:(CGRect)rect{
+    //如果数组中没有值, 则不进行绘制.
+    if(self.selectedButton.count <= 0) return;
+    //如果数组有值, 则进行绘制
+    UIBezierPath *drawPath = [UIBezierPath bezierPath];
+
+        for (int i = 0; i < self.selectedButton.count; i++)
+        {
+            UIButton *button = self.selectedButton[i];
+            if (i == 0) {
+
+                [drawPath moveToPoint:button.center];
+            }else
+            {
+                [drawPath addLineToPoint:button.center];
+            }
+
+        }
+
+
+    [drawPath addLineToPoint:self.currentPoint];
+    [drawPath setLineWidth:10];
+    [drawPath setLineJoinStyle:kCGLineJoinRound];
+
+    [[UIColor orangeColor] set];
+    [drawPath stroke];
 
 }
 @end
