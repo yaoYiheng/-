9209b929d14fa1ec06c56e85580fab40bc09c442
YihengYao <fn1117@qq.com>
2017-01-06 09:25:54 +1300

    17_01_06_心跳效果

--- 17_01_05_CABasciAnimation/17_01_05_CABasciAnimation/ViewController.m
+++ 17_01_05_CABasciAnimation/17_01_05_CABasciAnimation/ViewController.m
@@ -8,8 +8,12 @@
 
 #import "ViewController.h"
 
+#define angleToRadio(angle) ((angle) / 180.0 * M_PI)
+
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *orangeView;
+@property (weak, nonatomic) IBOutlet UIImageView *heartImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *ginImageView;
 
 @end
 
@@ -17,7 +21,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.ginImageView.layer.borderWidth = 8;
+    self.ginImageView.layer.borderColor = [UIColor orangeColor].CGColor;
+    self.ginImageView.layer.cornerRadius = 100;
+
+    self.ginImageView.layer.masksToBounds = YES;
+
 }
 
 
@@ -30,18 +40,39 @@
     CABasicAnimation *animation = [CABasicAnimation animation];
 
 //    animation.keyPath = @"hidden";
-//    animation.toValue = @YES;
+////    animation.toValue = @YES;
+//
+//    animation.keyPath = @"backgroundColor";
+//    animation.toValue = [UIColor redColor];
+//    animation.keyPath = @"position.y";
+//    animation.toValue = @400;
+//
+        //动画完成是是否移除动画
+//    animation.removedOnCompletion = NO;
+        //完成后动画保持什么状态
+//    animation.fillMode = @"forwards";
+//
+//      将动画添加到需要实现动画的层
+//    [self.orangeView.layer addAnimation:animation forKey:nil];
 
-    animation.keyPath = @"backgroundColor";
-    animation.toValue = [UIColor redColor];
-    animation.keyPath = @"position.y";
-    animation.toValue = @400;
+    /**
+     通过修改transform.scale, 实现心跳效果
+     */
+    animation.keyPath = @"transform.scale";
+    animation.toValue = @(0.5);
 
-    animation.removedOnCompletion = NO;
-    animation.fillMode = @"forwards";
+    //动画执行时长
+    animation.duration = 1;
+    //动画重复次数
+    animation.repeatCount = MAXFLOAT;
+    //怎么去怎么样回
+    animation.autoreverses = YES;
+
+    [self.heartImageView.layer addAnimation:animation forKey:nil];
+//
 
 
-    [self.orangeView.layer addAnimation:animation forKey:nil];
+
 }
 
 
