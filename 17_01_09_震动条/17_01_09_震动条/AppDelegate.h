e8e54f1fb9019bb7613a4c164e586c373444023b
YihengYao <fn1117@qq.com>
2017-01-09 09:21:16 +1300

    17_01_09_震动条
    知识点: CAReplicatorLayer 复制层, 用来复制需要做动画的图层, 可以通过设置其属性修改复制层动画延迟, 复制层的复制个数,
    以及instanceTransform 属性, 修改与transform相关的属性的效果

---
+++ 17_01_09_震动条/17_01_09_震动条/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  17_01_09_震动条
+//
+//  Created by 姚懿恒 on 2017/1/9.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 17_01_09_震动条/17_01_09_震动条/ViewController.m
@@ -0,0 +1,70 @@
+//
+//  ViewController.m
+//  17_01_09_震动条
+//
+//  Created by 姚懿恒 on 2017/1/9.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+
+
+}
+
+#pragma mark -添加复制层动画.
+- (void)configureReplicatorLayer
+{
+
+    //创建复制层.
+    CAReplicatorLayer *relL = [CAReplicatorLayer layer];
+    relL.frame = CGRectMake(50, 50, 300, 300);
+    relL.backgroundColor = [UIColor grayColor].CGColor;
+    [self.view.layer addSublayer:relL];
+
+    //设置需要做动画效果的图层, 并将其添加到复制层
+    CALayer *layer = [CALayer layer];
+    layer.bounds = CGRectMake(0, 0, 30, 150);
+    layer.backgroundColor = [UIColor orangeColor].CGColor;
+    layer.anchorPoint = CGPointMake(0, 1);
+    layer.position = CGPointMake(0, relL.frame.size.height);
+    [relL addSublayer:layer];
+
+    //复制层动画延迟
+    relL.instanceDelay = 0.3;
+    //复制包括自己在内的图层数
+    relL.instanceCount = 5;
+    //对每个复制的图层进行位移.
+    relL.instanceTransform = CATransform3DMakeTranslation(45, 0, 0);
+
+    //创建基本动画, 设置相关属性并将其添加到动画层
+    CABasicAnimation *animation = [CABasicAnimation animation];
+    animation.keyPath = @"transform.scale.y";
+    animation.toValue = @0;
+    animation.duration = 1;
+    animation.autoreverses = YES;
+    animation.repeatCount = MAXFLOAT;
+
+    [layer addAnimation:animation forKey:nil];
+
+
+
+
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
