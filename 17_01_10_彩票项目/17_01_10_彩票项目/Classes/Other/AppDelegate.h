407dc6545c79f66f1dd361a604c9eb51b67ef995
YihengYao <fn1117@qq.com>
2017-01-10 22:47:48 +1300

    17_01_10_基本的架构与结构的搭建

---
+++ 17_01_10_彩票项目/17_01_10_彩票项目/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  17_01_10_彩票项目
+//
+//  Created by 姚懿恒 on 2017/1/10.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 17_01_10_彩票项目/17_01_10_彩票项目/Classes/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  17_01_10_彩票项目
+//
+//  Created by 姚懿恒 on 2017/1/10.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
