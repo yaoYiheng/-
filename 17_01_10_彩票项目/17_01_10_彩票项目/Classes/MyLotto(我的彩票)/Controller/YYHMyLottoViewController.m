70f481e20d687b033ea2cb45c45d7fe4fa3516c6
YihengYao <fn1117@qq.com>
2017-01-12 23:31:49 +1300

    17_01_12_完成我的彩票
    知识点:1关于拉伸按钮的背景图片,
    [image stretchableImageWithLeftCapWidth:image.size.width / 2
    topCapHeight:image.size.height / 2]
    知识点2:设置完button的背景图片与文字 后需要设置其尺寸. 调用sizeToFit
        [button sizeToFit];

--- 17_01_10_彩票项目/17_01_10_彩票项目/Classes/MyLotto(我的彩票)/Controller/YYHMyLottoViewController.m
+++ 17_01_10_彩票项目/17_01_10_彩票项目/Classes/MyLotto(我的彩票)/Controller/YYHMyLottoViewController.m
@@ -9,6 +9,7 @@
 #import "YYHMyLottoViewController.h"
 
 @interface YYHMyLottoViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *LogInButton;
 
 @end
 
@@ -16,9 +17,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
 
+    //1.设置拉伸按钮图片
+    //1.1拿到按钮背景图片
+    UIImage *image = self.LogInButton.currentBackgroundImage;
+
+    image = [image stretchableImageWithLeftCapWidth:image.size.width / 2 topCapHeight:image.size.height / 2];
+    [self.LogInButton setBackgroundImage:image forState:UIControlStateNormal];
+
+    //2.设置导航栏左侧
+
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setImage:[UIImage imageNamed:@"FBMM_Barbutton"] forState:UIControlStateNormal];
+    [button setTitle:@"客服" forState:UIControlStateNormal];
+    //设置完button的背景图片与文字 后需要设置其尺寸. 调用sizeToFit
+    [button sizeToFit];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+
+    //3. 设置导航栏右侧
+    //因为有渲染效果, 所以调用之前写好的分类方法, 返回一张不带有渲染的图片
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageWithoutRendering:@"Mylottery_config"] style:0 target:self action:@selector(configure)];
+}
+- (void)configure{
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
