9c6bd969872669859c7ed0fb851d38e8fc98a025
YihengYao <fn1117@qq.com>
2017-01-22 10:25:08 +1300

    17_01_22_NSThread的基本使用
    知识点:
    
        //设置优先级  取值范围 0.0 ~ 1.0 之间 最高是1.0 默认优先级是0.5
        threadA.threadPriority = 1.0;
    /**
     分离一条子线程, 自动启动
     */
    - (void)creatThreadB{
        [NSThread detachNewThreadSelector:@selector(run) toTarget:self
    withObject:nil];
    
    }
    
    /**
     开启隐式线程, 注意调用的是控制器的方法, 而不是NSThread
     */
    - (void)creatThreadC{
        [self performSelectorInBackground:@selector(run) withObject:nil];
    }

--- 17_01_20_NSThread了解/17_01_20_NSThread了解/ViewController.m
+++ 17_01_20_NSThread了解/17_01_20_NSThread了解/ViewController.m
@@ -6,6 +6,16 @@
 //  Copyright © 2017年 姚懿恒. All rights reserved.
 //
 
+/**
+ 注意点:凡是和UI相关的操作都需要放到主线程中去处理
+ 不要将耗时的任务放到主线程中进行, 因为主线程主要处理UI事件, 耗时的
+ 任务会阻塞主线程, 不能及时处理UI事件, 给人以卡的感觉.
+ */
+//    for (int i = 0 ; i < 100000; i++) {
+//        NSLog(@"%d---%@", i, [NSThread mainThread]);
+//    }
+
+
 #import "ViewController.h"
 #import <pthread.h>
 
@@ -32,17 +42,53 @@
 }
 - (IBAction)btnClick:(id)sender {
 
-    /**
-     注意点:凡是和UI相关的操作都需要放到主线程中去处理
-     不要将耗时的任务放到主线程中进行, 因为主线程主要处理UI事件, 耗时的
-     任务会阻塞主线程, 不能及时处理UI事件, 给人以卡的感觉.
-     */
-//    for (int i = 0 ; i < 100000; i++) {
-//        NSLog(@"%d---%@", i, [NSThread mainThread]);
-//    }
+
+    [self ctreatNSThreadA];
 
 
+}
 
+/**
+ alloc init 创建主线程, 需手动开始主线程
+ */
+- (void)ctreatNSThreadA{
+    NSThread *threadA = [[NSThread alloc] initWithTarget:self selector:@selector(run) object:nil];
+    [threadA start];
+
+    //设置线层名
+    threadA.name = @"线程A";
+    
+    //设置优先级  取值范围 0.0 ~ 1.0 之间 最高是1.0 默认优先级是0.5
+    threadA.threadPriority = 1.0;
+
+    NSThread *threadB = [[NSThread alloc] initWithTarget:self selector:@selector(run) object:nil];
+    [threadB start];
+    threadB.name = @"线程B";
+    threadB.threadPriority = 0.1;
+
+    NSThread *threadC = [[NSThread alloc] initWithTarget:self selector:@selector(run) object:nil];
+    [threadC start];
+    threadC.name = @"线程C";
+    threadC.threadPriority = 0.5;
+}
+
+/**
+ 分离一条子线程, 自动启动
+ */
+- (void)creatThreadB{
+    [NSThread detachNewThreadSelector:@selector(run) toTarget:self withObject:nil];
+
+
+}
+
+
+/**
+ 开启隐式线程, 注意调用的是控制器的方法, 而不是NSThread
+ */
+- (void)creatThreadC{
+    [self performSelectorInBackground:@selector(run) withObject:nil];
+}
+- (void)creatPthread{
     //1.创建线程对象
     pthread_t threadA;
 
@@ -66,6 +112,7 @@
     int value = pthread_equal(threadA, threadB);
 
     NSLog(@"value === %d", value);
+
 }
 
 
@@ -76,12 +123,19 @@ void *sum(void *pa){
      将耗时的操作放到子线程中, 不影响UI操作.无卡顿感.
      
      */
-        for (int i = 0 ; i < 100000; i++) {
+        for (int i = 0 ; i < 100; i++) {
             NSLog(@"%d---%@", i, [NSThread mainThread]);
         }
     return NULL;
 }
 
+- (void)run{
+
+    for (NSInteger i = 0; i<100; i++) {
+        NSLog(@"%zd----%@",i,[NSThread currentThread].name);
+    }
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
