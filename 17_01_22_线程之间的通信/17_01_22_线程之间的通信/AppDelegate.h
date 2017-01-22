81f1caf4d90b345a79188da2cac13e577db1ca1a
YihengYao <fn1117@qq.com>
2017-01-22 22:05:35 +1300

    17_01_22_线程之间的通信

---
+++ 17_01_22_线程之间的通信/17_01_22_线程之间的通信/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  17_01_22_线程之间的通信
+//
+//  Created by 姚懿恒 on 2017/1/22.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 17_01_22_线程之间的通信/17_01_22_线程之间的通信/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  17_01_22_线程之间的通信
+//
+//  Created by 姚懿恒 on 2017/1/22.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
