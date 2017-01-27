655595ec737cce5ce918c76e07dc50049ec825f2
YihengYao <fn1117@qq.com>
2017-01-27 19:08:16 +1300

    17_01_27_ARC环境下实现单例模式

---
+++ 17_01_27_ARC环境下实现单例模式/17_01_27_ARC环境下实现单例模式/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17_01_27_ARC环境下实现单例模式
+//
+//  Created by 姚懿恒 on 2017/1/27.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 17_01_27_ARC环境下实现单例模式/17_01_27_ARC环境下实现单例模式/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  17_01_27_ARC环境下实现单例模式
+//
+//  Created by 姚懿恒 on 2017/1/27.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
