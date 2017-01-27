1454460f2fc16352618d5642a039afa1eca16290
YihengYao <fn1117@qq.com>
2017-01-28 00:00:04 +1300

    17_01_27_MRC环境下实现单例模式
    知识点:
    判断当前是不是ARC环境, 如果是就不需要对内存进行管理, 如果不是则执行内存管理代码
    
    —
    #if __has_feature(objc_arc)
    //条件满足 ARC就什么也不做
    
    #else
    //不然就执行以下代码
    
    新年快乐,鸡年大吉;)

---
+++ 17_01_27_MRC环境下实现单例模式/YYHTool.h
@@ -0,0 +1,15 @@
+//
+//  YYHTool.h
+//  17_01_27_ARC环境下实现单例模式
+//
+//  Created by 姚懿恒 on 2017/1/27.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YYHTool : NSObject
+
++ (instancetype)shareTool;
+
+@end
