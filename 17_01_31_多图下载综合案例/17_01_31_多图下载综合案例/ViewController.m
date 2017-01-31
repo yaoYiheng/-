930f333baf4fe08bd33dba8a584081d869803524
YihengYao <fn1117@qq.com>
2017-02-01 11:06:35 +1300

    17_02_01_解决图片重复下载
    在下载的时候, 将图片保存至字典.并在显示图片时, 进行判断是否图片已存在内存中.
    但是, 当下次进入时, 图片不存在, 所以需要将图片下载到沙盒中.

--- 17_01_31_多图下载综合案例/17_01_31_多图下载综合案例/ViewController.m
+++ 17_01_31_多图下载综合案例/17_01_31_多图下载综合案例/ViewController.m
@@ -14,6 +14,9 @@
 /** <#comments#>*/
 @property (nonatomic, strong) NSArray *dataArray;
 
+/** 图片字典*/
+@property (nonatomic, strong) NSMutableDictionary *images;
+
 @end
 
 @implementation ViewController
@@ -22,18 +25,28 @@
     [super viewDidLoad];
 }
 
+#pragma mark - lazy loading
+
+- (NSMutableDictionary *)images{
+    if (!_images) {
+        _images = [NSMutableDictionary dictionary];
+    }
+    return _images;
+}
 - (NSArray *)dataArray{
     if (!_dataArray) {
         _dataArray = [YYHAppItem mj_objectArrayWithFilename:@"apps.plist"];
     }
     return _dataArray;
 }
+#pragma mark dataSourse
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 1;
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return self.dataArray.count;
 }
+#pragma mark tableViewDelegate
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
 
     static NSString *ID = @"app";
@@ -45,12 +58,27 @@
     //根据模型设置cell
     cell.textLabel.text = appItem.name;
     cell.detailTextLabel.text = appItem.download;
+
+    //尝试从内存缓存中获取图片
+    UIImage *iconImage = self.images[appItem.icon];
+    if (iconImage) {
+        cell.imageView.image = iconImage;
+    }
+    else{
     //从网络下载图片
     NSURL *url = [NSURL URLWithString:appItem.icon];
     NSData *imageData = [NSData dataWithContentsOfURL:url];
-    UIImage *iconImage = [UIImage imageWithData:imageData];
-    
+    iconImage = [UIImage imageWithData:imageData];
+        //设置到cell
     cell.imageView.image = iconImage;
+
+    //如果内存中没有就将图片根据键保存到字典中.
+    [self.images setObject:iconImage forKey:appItem.icon];
+
+    }
+
+
+
 
     return cell;
 }
