7105733113fa0c7d26d94543e134d707ad9ecb44
YihengYao <fn1117@qq.com>
2017-02-05 23:06:15 +1300

    17_02_05_NSCache的基本使用

---
+++ 17_02_05_NSCache的基本使用/17_02_05_NSCache的基本使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  17_02_05_NSCache的基本使用
+//
+//  Created by 姚懿恒 on 2017/2/5.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 17_02_05_NSCache的基本使用/17_02_05_NSCache的基本使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17_02_05_NSCache的基本使用
+//
+//  Created by 姚懿恒 on 2017/2/5.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
