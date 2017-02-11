268aa72a5729d9348f70812ea7eeac3763b202bf
YihengYao <fn1117@qq.com>
2017-02-11 23:20:33 +1300

    17_02_11_OC对象转为JSON
    知识点:
        //将字符串转为JSON数据, 并不能将字符串转为JSON数据
        /*
         - Top level object is an NSArray or NSDictionary
         - All objects are NSString, NSNumber, NSArray, NSDictionary, or
    NSNull
         - All dictionary keys are NSStrings
         - NSNumbers are not NaN or infinity
    
         - 最外层必须是 NSArray or NSDictionary
         - 所有的元素必须是 NSString, NSNumber, NSArray, NSDictionary, or NSNull
         - 字典中所有的key都必须是 NSStrings类型的
         - NSNumbers不能死无穷大
         */
    
    选项NSJSONWritingPrettyPrinted 排版 美观

--- 17_02_11_JSON解析/17_02_11_JSON解析/ViewController.m
+++ 17_02_11_JSON解析/17_02_11_JSON解析/ViewController.m
@@ -14,16 +14,54 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+
+    [self ocToJSON];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+- (void)ocToJSON{
 
+    //将字典转为JSON数据
+    NSDictionary *dict = @{@"name": @"pig",
+                           @"age" :@23};
+    /*
+     第一个参数:要转换的OC对象
+     第二个参数:选项NSJSONWritingPrettyPrinted 排版 美观
+     */
+    NSData *data = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
+
+    NSLog(@"%@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+
+    //将数组转为JSON数据
+    NSArray *array = @[@"123",@"234"];
+    NSData *data2 = [NSJSONSerialization dataWithJSONObject:array options:NSJSONWritingPrettyPrinted error:nil];
+
+    NSLog(@"%@", [[NSString alloc]initWithData:data2 encoding:NSUTF8StringEncoding]);
+
+    //将字符串转为JSON数据, 并不能将字符串转为JSON数据
+    /*
+     - Top level object is an NSArray or NSDictionary
+     - All objects are NSString, NSNumber, NSArray, NSDictionary, or NSNull
+     - All dictionary keys are NSStrings
+     - NSNumbers are not NaN or infinity
+     
+     - 最外层必须是 NSArray or NSDictionary
+     - 所有的元素必须是 NSString, NSNumber, NSArray, NSDictionary, or NSNull
+     - 字典中所有的key都必须是 NSStrings类型的
+     - NSNumbers不能死无穷大
+     */
+
+    NSString *string = @"hahahha";
+    if(![NSJSONSerialization isValidJSONObject:string] ){
+
+        NSLog(@"该类型不能转化为JSON数据");
+        return;
+    }
+    NSData *data3 = [NSJSONSerialization dataWithJSONObject:string options:NSJSONWritingPrettyPrinted error:nil];
+
+    NSLog(@"%@", [[NSString alloc]initWithData:data3 encoding:NSUTF8StringEncoding]);
+
+}
 
 @end
