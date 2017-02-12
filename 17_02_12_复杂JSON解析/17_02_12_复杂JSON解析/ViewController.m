7666b800d217c1a7118495ca28452f833094f0c2
YihengYao <fn1117@qq.com>
2017-02-12 23:41:48 +1300

    17_02_12_为了避免框架文件太多, 先提交

--- 17_02_12_复杂JSON解析/17_02_12_复杂JSON解析/ViewController.m
+++ 17_02_12_复杂JSON解析/17_02_12_复杂JSON解析/ViewController.m
@@ -9,20 +9,61 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+/** <#comments#>*/
+@property (nonatomic, strong) NSArray *dataArray;
 @end
 
 @implementation ViewController
+-(NSArray *)dataArray{
+    if (!_dataArray) {
+        _dataArray = [NSArray array];
+    }
+    return _dataArray;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //从网络获JSON数据
+    //1. 确定URL
+    NSURL *url = [NSURL URLWithString:@"http://120.25.226.186:32812/video?type=JSON"];
+    //2. 创建请求体
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    //3. 发送异步请求
+    [NSURLConnection sendAsynchronousRequest:request queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        //拿到数据后将data转为对象
+
+
+        //容错处理
+        if(connectionError) return ;
+
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+
+        self.dataArray = dict[@"video"];
+
+        //刷新tableView, 回到主线程中
+        [self performSelector:@selector(reloadData) onThread:[NSThread mainThread] withObject:nil waitUntilDone:NO];
+    }];
 }
 
+#pragma mark -------
+#pragma mark UITabelViewSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.dataArray.count;
+}
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *ID = @"vedio";
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    //1.从缓存中获取cell
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+
+    NSDictionary *dict = self.dataArray[indexPath.row];
+    //2. 设置cell
+    cell.textLabel.text = dict[@"name"];
+    cell.detailTextLabel.text = dict[@"length"];
+    //2.1 设置图片.
+
 }
 
 
