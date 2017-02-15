e17b44535bd8cf1c3c7abc9e5581ce69bce01866
YihengYao <fn1117@qq.com>
2017-02-16 12:29:56 +1300

    17_02_16_大文件下载
    知识点:
    
        //通过文件管理者这个类, 创建一个空的文件, 路径为指定路径.
        [[NSFileManager defaultManager] createFileAtPath:self.fielPath
    contents:nil attributes:nil];
    
        //创建文件句柄(用于指向文件末尾的指针)
        self.handle = [NSFileHandle
    fileHandleForWritingAtPath:self.fielPath];
    
        //将文件句柄移动到文件末端
        [self.handle seekToEndOfFile];
    
        //将文件写入到句柄后
        [self.handle writeData:data];

--- 17_02_16_NSURLConnection实现大文件下载/17_02_16_NSURLConnection实现大文件下载/ViewController.m
+++ 17_02_16_NSURLConnection实现大文件下载/17_02_16_NSURLConnection实现大文件下载/ViewController.m
@@ -7,23 +7,84 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
+@interface ViewController () <NSURLConnectionDataDelegate>
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
+/** 文件大小*/
+@property (nonatomic, assign) NSInteger totalFile;
+/** 文件路径*/
+@property (nonatomic, strong) NSString *fielPath;
+/** 当前进度*/
+@property (nonatomic, assign) NSInteger currentProgress;
 
-@interface ViewController ()
+/** 文件句柄*/
+@property (nonatomic, strong) NSFileHandle *handle;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+
+    //1. 确定URL
+    NSURL *url = [NSURL URLWithString:@"http://120.25.226.186:32812/resources/videos/minion_01.mp4"];
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    [[NSURLConnection alloc] initWithRequest:request delegate:self];
+
+
 }
+//收到来自服务器的响应时调用.
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
+
+    //确定文件的总大小.
+    self.totalFile = response.expectedContentLength;
+
+    //确定写入路径
+
+    self.fielPath = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"1.mp4"];;
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+    //通过文件管理者这个类, 创建一个空的文件, 路径为指定路径.
+    [[NSFileManager defaultManager] createFileAtPath:self.fielPath contents:nil attributes:nil];
+
+    //创建文件句柄(用于指向文件末尾的指针)
+    self.handle = [NSFileHandle fileHandleForWritingAtPath:self.fielPath];
+
+
+
 }
+//接收到来自服务器的数据时调用, 会调用多次.
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+
+    //将文件句柄移动到文件末端
+    [self.handle seekToEndOfFile];
+
+    //将文件写入到句柄后
+    [self.handle writeData:data];
+
+    //设置当前进度
+    self.currentProgress += data.length;
+
+    NSLog(@"%f", 1.0 * self.currentProgress / self.totalFile);
+
+    //设置进度条
+    self.progressView.progress = 1.0 * self.currentProgress / self.totalFile;
 
 
+}
+//结束传输时调用.
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    //关闭文件句柄
+    [self.handle closeFile];
+
+    self.handle = nil;
+
+}
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    
+}
 @end
