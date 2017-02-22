2e2a1234fa1d288befd70b16f106649602162be1
YihengYao <fn1117@qq.com>
2017-02-22 18:01:14 +1300

    17_02_22_NSURLSession下载大文件_frame

---
+++ 17_02_22_NSURLSession下载大文件/17_02_22_NSURLSession下载大文件/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  17_02_22_NSURLSession下载大文件
+//
+//  Created by 姚懿恒 on 2017/2/22.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
