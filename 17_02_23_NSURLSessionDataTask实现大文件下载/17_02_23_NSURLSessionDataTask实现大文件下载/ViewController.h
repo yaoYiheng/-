51410215daa69ae2459c659008e15d8d9c603102
YihengYao <fn1117@qq.com>
2017-02-23 23:32:21 +1300

    17_02_23_NSURLSessionDataTask实现大文件下载_frame

---
+++ 17_02_23_NSURLSessionDataTask实现大文件下载/17_02_23_NSURLSessionDataTask实现大文件下载/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17_02_23_NSURLSessionDataTask实现大文件下载
+//
+//  Created by 姚懿恒 on 2017/2/23.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 17_02_23_NSURLSessionDataTask实现大文件下载/17_02_23_NSURLSessionDataTask实现大文件下载/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  17_02_23_NSURLSessionDataTask实现大文件下载
+//
+//  Created by 姚懿恒 on 2017/2/23.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+}
+
+
+@end
