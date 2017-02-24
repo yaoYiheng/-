66d5fe88e2b6b3e5bd84649dab96f3f0b2f45bc6
YihengYao <fn1117@qq.com>
2017-02-24 23:55:57 +1300

    17_02_24_NSURLSessionDataTask离线下载_frame

---
+++ 17_02_24_NSURLSessionDataTask离线下载/17_02_24_NSURLSessionDataTask离线下载/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17_02_24_NSURLSessionDataTask离线下载
+//
+//  Created by 姚懿恒 on 2017/2/24.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 17_02_24_NSURLSessionDataTask离线下载/17_02_24_NSURLSessionDataTask离线下载/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  17_02_24_NSURLSessionDataTask离线下载
+//
+//  Created by 姚懿恒 on 2017/2/24.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
+
+@end
+
+@implementation ViewController
+- (IBAction)startClick:(id)sender {
+}
+- (IBAction)cancelClick:(id)sender {
+}
+
+
+- (IBAction)goOnClick:(id)sender {
+}
+- (IBAction)pauseClick:(id)sender {
+}
+
+@end
