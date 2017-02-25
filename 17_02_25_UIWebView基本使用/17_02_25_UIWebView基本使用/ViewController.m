83e2019bf25dd652cb4e8643a2d55c95871e7ff5
YihengYao <fn1117@qq.com>
2017-02-25 23:25:56 +1300

    17_02_25_UIWebView基本使用_frame

---
+++ 17_02_25_UIWebView基本使用/17_02_25_UIWebView基本使用/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  17_02_25_UIWebView基本使用
+//
+//  Created by 姚懿恒 on 2017/2/25.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self test1];
+}
+
+@end
