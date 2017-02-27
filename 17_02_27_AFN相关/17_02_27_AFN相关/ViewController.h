4ec6595b93795c7d7a0677838a31f4e015e4ab13
YihengYao <fn1117@qq.com>
2017-02-27 23:22:02 +1300

    17_02_27_AFN相关_frame

---
+++ 17_02_27_AFN相关/17_02_27_AFN相关/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17_02_27_AFN相关
+//
+//  Created by 姚懿恒 on 2017/2/27.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 17_02_27_AFN相关/17_02_27_AFN相关/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  17_02_27_AFN相关
+//
+//  Created by 姚懿恒 on 2017/2/27.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AFNetworking.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+
+}
+
+@end
