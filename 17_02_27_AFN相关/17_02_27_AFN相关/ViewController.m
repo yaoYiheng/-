83e15065d2b39e023692da048493a2d3eedecf3b
YihengYao <fn1117@qq.com>
2017-02-28 15:30:25 +1300

    17_02_28_AFN上传, 解析JSON与XML
    知识点:
    在使用AFN进行文件上传时, 不建议使用    NSURLSessionUploadTask *uploadTask = [manager
    uploadTaskWithRequest:request fromData:[self getBodyData]
    progress:^(NSProgress * _Nonnull uploadProgress) {
            NSLog(@"%f",1.0 * uploadProgress.completedUnitCount/
    uploadProgress.totalUnitCount);
    
        } completionHandler:^(NSURLResponse * _Nonnull response, id
    _Nullable responseObject, NSError * _Nullable error) {
    
            NSLog(@"%@",responseObject);
        }];
    该方法进行文件上传,
    而是采用uploadWithAFN的做法
    在 第三个参数:constructingBodyWithBlock 处理要上传的文件数据, 对于需要上传的文件, 使用[formData
    append….] 具体使用.
    
    ==============
    AFN默认采用JSON的解析方案,     //如果返回数据类型是XML,
    那么应该修改解析方案为AFXMLParserResponseSerializer
        manager.responseSerializer = [AFXMLParserResponseSerializer
    serializer];
    
        //注意: 如果返回的数据类型既不是JSON, 也不是XML, 那么解析方案应该改为AFHTTPResponseSerializer
    
    ——————
    AFN默认能解析的数据有
    
     - `application/json`
     - `text/json`
     - `text/javascript`
    
    如果想要AFN能够解析别的数据类型, 需要设置
    
        //告诉AFN能够接受text/html类型的数据
        manager.responseSerializer.acceptableContentTypes = [NSSet
    setWithObject:@"text/html"];

--- 17_02_27_AFN相关/17_02_27_AFN相关/ViewController.m
+++ 17_02_27_AFN相关/17_02_27_AFN相关/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "AFNetworking.h"
 
-@interface ViewController ()
+@interface ViewController ()<NSXMLParserDelegate>
 
 @end
 
@@ -20,7 +20,7 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
 
-    [self downloadWithAFN];
+    [self xml];
 }
 
 /**
@@ -100,6 +100,8 @@
     [dataTask resume];
 
 }
+
+
 - (void)downloadWithAFN{
 
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
@@ -142,9 +144,148 @@
 
     [downloadTask resume];
 }
+
+/**
+ 推荐使用
+ */
 - (void)uploadWithAFN{
+
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+
+
+    /*
+     第一个参数:请求路径
+     第二个参数:字典(非文件参数)
+     第三个参数:constructingBodyWithBlock 处理要上传的文件数据
+     第四个参数:进度回调
+     第五个参数:成功回调 responseObject:响应体信息
+     第六个参数:失败回调
+     */
+    NSURLSessionDataTask *dataTask = [manager POST:@"http://120.25.226.186:32812/upload" parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+
+
+        /*
+         对于需要上传的文件, 使用 formData进行拼接, 而不需要像NSURLSessionUploadTask
+         对需要上传的文件进行繁琐的拼接过程
+         */
+
+        /*通过传入文件所在的URL进行文件上传*/
+        [formData appendPartWithFileURL:[NSURL fileURLWithPath:@"/Users/Morris/Documents/图形文件/银河/6.jpg"] name:@"file" error:nil];
+
+        //使用formData来拼接数据
+        /*
+         第一个参数:二进制数据 要上传的文件参数
+         第二个参数:服务器规定的
+         第三个参数:该文件上传到服务器以什么名称保存
+         */
+        //[fo
+
+        UIImage *iamge = [UIImage imageNamed:@"6.jpg"];
+        NSData *imageData = UIImageJPEGRepresentation(iamge, 0);
+
+        [formData appendPartWithFileData:imageData name:@"file" fileName:@"123.jpg" mimeType:@"image/jpg"];
+    } progress:^(NSProgress * _Nonnull uploadProgress) {
+        nil;
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"%@", responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        nil;
+    }];
+
+
+    [dataTask resume];
+}
+
+/**
+ 返回的数据类型为JSON
+ */
+- (void)json{
+
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+
+    NSDictionary *paramers = @{
+                               @"username":@"520it",
+                               @"pwd":@"520it",
+                               @"type":@"JSON"};
+    [manager GET:@"http://120.25.226.186:32812/login" parameters:paramers progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"%@", responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@ --- 请求失败", error);
+    }];
+
+}
+- (void)xml
+{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+
+    //如果返回数据类型是XML, 那么应该修改解析方案为AFXMLParserResponseSerializer
+    manager.responseSerializer = [AFXMLParserResponseSerializer serializer];
+
+    //注意: 如果返回的数据类型既不是JSON, 也不是XML, 那么解析方案应该改为AFHTTPResponseSerializer
+
+    NSDictionary *paramers = @{
+                               @"type":@"XML"
+                               };
+    [manager GET:@"http://120.25.226.186:32812/login" parameters:paramers progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        /*
+         由于解析方案为AFXMLParserResponseSerializer, 所以返回的responseObject也是NSXMLParser类型的,以下就是对其解析的方法
+         */
+
+        NSXMLParser *parser = responseObject;
+        parser.delegate = self;
+
+        [parser parse];
+        NSLog(@"%@", responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"请求失败");
+    }];
+
+}
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary<NSString *,NSString *> *)attributeDict
+{
+    NSLog(@"%@------", [attributeDict class]);
+}
+
+- (void)httpData{
+    //1.创建会话管理者
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+
+    //注意:如果返回的数据既不是xml也不是json那么应该修改解析方案为:
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+
+    //2.发送GET请求
+    [manager GET:@"http://120.25.226.186:32812/resources/images/minion_01.png" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task,id  _Nullable responseObject) {
+        NSLog(@"%@-",[responseObject class]);
+
+        //UIImage *image = [UIImage imageWithData:responseObject];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"请求失败--%@",error);
+    }];
+
 
 }
 
+-(void)httpData2
+{
+    //1.创建会话管理者
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+
+    //告诉AFN能够接受text/html类型的数据
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+
+    //注意:如果返回的数据既不是xml也不是json那么应该修改解析方案为:
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+
+    //2.发送GET请求
+    [manager GET:@"http://www.baidu.com" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task,id  _Nullable responseObject)
+    {
+        //解析数据.
+        NSLog(@"%@-%@",[responseObject class],[[NSString alloc]initWithData:responseObject encoding:NSUTF8StringEncoding]);
+
+        //UIImage *image = [UIImage imageWithData:responseObject];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"请求失败--%@",error);
+    }];
+}
 
 @end
