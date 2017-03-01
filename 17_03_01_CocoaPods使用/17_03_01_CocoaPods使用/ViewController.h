f49087aab7aef14ff74ab6b4e9209c62cdd63503
YihengYao <fn1117@qq.com>
2017-03-01 23:53:51 +1300

    17_03_01_使用CocoaPods
    知识点:
    Podfile 配置文件固定语句（如下）：
    ```说明：可能有些人看到的固定语句不一样，但是大同小异，基本上都能实现最终效果。`
    --->
    # Uncomment this line to define a global platform for your project
    platform :ios, '8.0'
    target 'CocoaPodsTestDemo' do
    # Uncomment this line if you're using Swift or would like to use
    dynamic frameworks
    # Pods for CocoaPodsTestDemo
     pod 'AFNetworking', '~>3.1.0'
     pod 'SDWebImage'
     pod 'MBProgressHUD'
     pod 'FMDB'
     pod 'Masonry'
     pod 'MJExtension'
     pod 'MJRefresh'
    # use_frameworks!
    # pod 'ReactiveCocoa'
    end
    
    =========
    8.安装
    pod install --no-repo-update
    pod update --no-repo-update

---
+++ 17_03_01_CocoaPods使用/17_03_01_CocoaPods使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17_03_01_CocoaPods使用
+//
+//  Created by 姚懿恒 on 2017/3/1.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 17_03_01_CocoaPods使用/Pods/Target Support Files/Pods-17_03_01_CocoaPods使用/Pods-17_03_01_CocoaPods使用-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_17_03_01_CocoaPods__ : NSObject
+@end
+@implementation PodsDummy_Pods_17_03_01_CocoaPods__
+@end
