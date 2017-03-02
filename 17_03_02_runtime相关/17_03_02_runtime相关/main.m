7a8ba5bb7e82a54a37ba8bb07a36f743bb91f097
YihengYao <fn1117@qq.com>
2017-03-02 21:58:59 +1300

    17_03_02_runtime相关_frame

---
+++ 17_03_02_runtime相关/17_03_02_runtime相关/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  17_03_02_runtime相关
+//
+//  Created by 姚懿恒 on 2017/3/2.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
