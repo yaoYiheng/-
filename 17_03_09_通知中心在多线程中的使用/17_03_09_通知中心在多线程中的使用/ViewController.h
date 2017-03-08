19bfc992a3ff7baed3571caaf6b8e60f7118638c
YihengYao <fn1117@qq.com>
2017-03-09 11:45:06 +1300

    17_03_09_通知中心在多线程中的使用_frame

---
+++ 17_03_09_通知中心在多线程中的使用/17_03_09_通知中心在多线程中的使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17_03_09_通知中心在多线程中的使用
+//
+//  Created by 姚懿恒 on 2017/3/9.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 17_03_09_通知中心在多线程中的使用/17_03_09_通知中心在多线程中的使用/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  17_03_09_通知中心在多线程中的使用
+//
+//  Created by 姚懿恒 on 2017/3/9.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
