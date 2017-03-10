b3cf6b23a02b68332a6d01338ba21ae01eb7e1e9
YihengYao <fn1117@qq.com>
2017-03-10 19:35:22 +1300

    17_03_10_block相关_frame

---
+++ 17_03_10_block相关/17_03_10_block相关/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  17_03_10_block相关
+//
+//  Created by 姚懿恒 on 2017/3/10.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
