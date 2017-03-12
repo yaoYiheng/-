7969669d33c4261acf7265a88729eb767048652b
YihengYao <fn1117@qq.com>
2017-03-12 21:31:49 +1300

    7_03_12_block作为参数使用_frame

---
+++ 17_03_12_block作为参数使用/17_03_12_block作为参数使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  17_03_12_block作为参数使用
+//
+//  Created by 姚懿恒 on 2017/3/12.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 17_03_12_block作为参数使用/17_03_12_block作为参数使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17_03_12_block作为参数使用
+//
+//  Created by 姚懿恒 on 2017/3/12.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
