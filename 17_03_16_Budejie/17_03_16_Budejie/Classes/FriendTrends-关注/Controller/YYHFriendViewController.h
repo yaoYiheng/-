a91133dfc360f6bcb04731f8aeb77435c1381f21
YihengYao <fn1117@qq.com>
2017-03-16 23:46:39 +1300

    创建各子控制器

---
+++ 17_03_16_Budejie/17_03_16_Budejie/Classes/FriendTrends-关注/Controller/YYHFriendViewController.h
@@ -0,0 +1,13 @@
+//
+//  YYHFriendViewController.h
+//  17_03_16_Budejie
+//
+//  Created by 姚懿恒 on 2017/3/16.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYHFriendViewController : UIViewController
+
+@end
