e4cffcdee25584de8e2c602e25f6499d63dbe512
YihengYao <fn1117@qq.com>
2017-03-17 00:04:25 +1300

    创建各个导航控制器与相关内容的控制器以及各个barItem的图标与文字

--- 17_03_16_Budejie/17_03_16_Budejie/Classes/Other-其他/AppDelegate.m
+++ 17_03_16_Budejie/17_03_16_Budejie/Classes/Other-其他/AppDelegate.m
@@ -10,6 +10,12 @@
 
 
 #import "AppDelegate.h"
+//导入自定义控制器头文件
+#import "YYHEssenceViewController.h"
+#import "YYHFriendViewController.h"
+#import "YYHNewViewController.h"
+#import "YYHPublishViewController.h"
+#import "YYHMeTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -19,7 +25,78 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+
+    //1. 创建窗口
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    //2. 设置窗口根控制器
+
+    /*
+     分析: 
+     该应该的结构应该是一个UItabBarViewController与 UINavigationController组合的主流框架,
+     创建一个UItabBarViewController, 然后为其添加子控制器UINavigationController, 再讲每个模块的控制器设置为UINavigationController的子控制器, 这样就能呈现的是上下方都有条的框架
+     */
+
+    //创建根控制器
+    UITabBarController *tabBarVC = [[UITabBarController alloc] init];
+
+    //创建各个导航控制器与相关内容的控制器
+    //精华
+    YYHEssenceViewController *essVC = [[YYHEssenceViewController alloc] init];
+    UINavigationController *essNav = [[UINavigationController alloc] initWithRootViewController:essVC];
+    [tabBarVC addChildViewController:essNav];
+    //新帖
+    YYHNewViewController *newVC = [[YYHNewViewController alloc] init];
+    UINavigationController *newNav = [[UINavigationController alloc] initWithRootViewController:newVC];
+    [tabBarVC addChildViewController:newNav];
+    //发布 ->不需要加入导航控制器
+    YYHPublishViewController *publicVC = [[YYHPublishViewController alloc] init];
+
+    [tabBarVC addChildViewController:publicVC];
+    //关注
+    YYHFriendViewController *friendVC = [[YYHFriendViewController alloc] init];
+    UINavigationController *friendNav = [[UINavigationController alloc] initWithRootViewController:friendVC];
+    [tabBarVC addChildViewController:friendNav];
+    //我
+    YYHMeTableViewController *meVC = [[YYHMeTableViewController alloc] init];
+    UINavigationController *meNav = [[UINavigationController alloc] initWithRootViewController:meVC];
+    [tabBarVC addChildViewController:meNav];
+
+
+
+    //设置各个barItem的图标与文字
+
+    essNav.tabBarItem.title = @"精华";
+    essNav.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
+    essNav.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
+
+    // 1:新帖
+    newNav.tabBarItem.title = @"新帖";
+    newNav.tabBarItem.image = [UIImage imageNamed:@"tabBar_new_icon"];
+    newNav.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_new_click_icon"];
+
+    // 2:发布
+    publicVC.tabBarItem.image = [UIImage imageNamed:@"tabBar_publish_icon"];
+    publicVC.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_publish_click_icon"];
+
+    // 3.关注
+    friendNav.tabBarItem.title = @"关注";
+    friendNav.tabBarItem.image = [UIImage imageNamed:@"tabBar_friendTrends_icon"];
+    friendNav.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_friendTrends_click_icon"];
+
+    // 4.我
+    meNav.tabBarItem.title = @"我";
+    meNav.tabBarItem.image = [UIImage imageNamed:@"tabBar_me_icon"];
+    meNav.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_me_click_icon"];
+
+
+
+    //设置为窗口跟控制器
+    self.window.rootViewController = tabBarVC;
+
+    //3. 使窗口可见
+    [self.window makeKeyAndVisible];
+
     return YES;
 }
 
