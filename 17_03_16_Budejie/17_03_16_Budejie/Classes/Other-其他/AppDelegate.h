31b80099f6419f6e9df0bdec6b9e0a9d506277bc
YihengYao <fn1117@qq.com>
2017-03-16 23:42:47 +1300

    17_03_16_不得姐项目_设图标以及运行前图片
    知识点:
    /*
     优先级:LaunchScreen > LaunchImage
     在xcode配置了,不起作用 1.清空xcode缓存 2.直接删掉程序 重新运行
     如果是通过LaunchImage设置启动界面,那么屏幕的可视范围由图片决定
     注意:如果使用LaunchImage,必须让你的美工提供各种尺寸的启动图片
    
     LaunchScreen:Xcode6开始才有
     LaunchScreen好处:1.自动识别当前真机或者模拟器的尺寸 2.只要让美工提供一个可拉伸图片
     3.展示更多东西
    
     LaunchScreen底层实现:把LaunchScreen截屏,生成一张图片.作为启动界面
    
     */

---
+++ 17_03_16_Budejie/17_03_16_Budejie/Classes/Other-其他/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  17_03_16_Budejie
+//
+//  Created by 姚懿恒 on 2017/3/16.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
