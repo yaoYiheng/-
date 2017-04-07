f05ad9f7a715f09813c4bb2496d010247b57c310
YihengYao <fn1117@qq.com>
2017-04-07 22:49:00 +1200

    导入图片

---
+++ 17_03_16_Budejie/17_03_16_Budejie/Classes/Essence-精华/View/YYHTopicCellTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  YYHTopicCellTableViewCell.h
+//  17_03_16_Budejie
+//
+//  Created by 姚懿恒 on 2017/4/7.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYHTopicCellTableViewCell : UITableViewCell
+
+@end
---
+++ 17_03_16_Budejie/17_03_16_Budejie/Classes/Essence-精华/View/YYHTopicCellTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  YYHTopicCellTableViewCell.m
+//  17_03_16_Budejie
+//
+//  Created by 姚懿恒 on 2017/4/7.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#import "YYHTopicCellTableViewCell.h"
+
+@implementation YYHTopicCellTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
